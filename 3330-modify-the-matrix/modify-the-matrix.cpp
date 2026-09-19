bd890ba57236322cf3ca8d9e881b3097e19cb254
Roman <romanthapliyal2@gmail.com>
2026-09-20 00:03:02 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (37.57%) - LeetSync

---
+++ 3330-modify-the-matrix/modify-the-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
+        
+        for(int j=0;j<matrix[0].size();j++){
+            int max=INT_MIN;
+            for(int i=0;i<matrix.size();i++){
+                if(max<matrix[i][j]){max=matrix[i][j];}
+            }
+            for(int i=0;i<matrix.size();i++){
+                if(matrix[i][j]==-1){matrix[i][j]=max;}
+            }
+        }
+        return matrix;
+    }
+};
