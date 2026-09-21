295516175e7e8078d4b81e43c8fd0568b4e43db3
Roman <romanthapliyal2@gmail.com>
2026-09-22 00:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (33.68%) - LeetSync

--- 2723-find-the-longest-balanced-substring-of-a-binary-string/find-the-longest-balanced-substring-of-a-binary-string.cpp
+++ 2723-find-the-longest-balanced-substring-of-a-binary-string/find-the-longest-balanced-substring-of-a-binary-string.cpp
@@ -4,22 +4,19 @@ public:
         int n=s.size();
         if(n==0){return n;}
         int maxLen=0;
-        for(int i=0;i<n;i++){
-            int c1=0,c0=0;
-            bool seenOne=false;
-            for(int j=i;j<n;j++){
-                if(s[j]=='0'){
-                    c0++;
-                    if(seenOne){break;}
-                }
-                else{
-                    seenOne=true;
-                    c1++;
-                }
-                int len=j-i+1;
-                if(len%2!=0){continue;}
-                if(c0==c1){maxLen=max(maxLen,len);}
+        int i=0;
+        while(i<n){
+            int c0=0,c1=0;
+            while(i<n&&s[i]=='0'){
+                c0++;
+                i++;
             }
+            while(i<n&&s[i]=='1'){
+                c1++;
+                i++;
+            }
+            int balancedLen=2*min(c0,c1);
+            maxLen=max(maxLen,balancedLen);
         }
         return maxLen;
     }
